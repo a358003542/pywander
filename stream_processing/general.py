b9dd55b7411253625eb7de20cd16014d8a92e7e5
wanze <a358003542@outlook.com>
2019-03-15 10:31:00 +0800

    流处理函数执行顺序调整为从左到右

--- stream_processing/general.py
+++ stream_processing/general.py
@@ -14,14 +14,15 @@ def build_compose_function(*funcs):
     :param args:
     :return:
     """
-    return reduce(lambda f, g: lambda *args, **kwargs: f(g(*args, **kwargs)), funcs)
+    return reduce(lambda f, g: lambda *args, **kwargs: g(f(*args, **kwargs)), funcs)
 
 
 def build_stream_function(*funcs):
     """
     构建流处理函数 函数参数更严格 只接受一个参数 d 字典值
+    函数执行的顺序是从左到右
     :param funcs:
     :return:
     """
 
-    return reduce(lambda f, g: lambda d: f(g(d)), funcs)
+    return reduce(lambda f, g: lambda d: g(f(d)), funcs)
