ac47f0aa7f5bbbc5b08a6eb0f823ab0ab4dfe944
wanze <a358003542@outlook.com>
2019-03-27 17:49:16 +0800

    go

--- database/__init__.py
+++ database/__init__.py
@@ -3,8 +3,10 @@
 
 
 from .mongodb.conn import get_mongodb_client
+
 from .sqldb.conn import create_sqlalchemy_url
 from .sqldb.base import SQLDataBase
+
 __all__ = [
     'get_mongodb_client',
     'create_sqlalchemy_url',
--- examples/load_exists_sqldb.py
+++ examples/load_exists_sqldb.py
@@ -5,15 +5,12 @@
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
 
-from database.sqldb import SQLDataBase
-
+from mymodule.database import SQLDataBase
 
 sqldb_lianzai = SQLDataBase('your_sqlalchemy_conn', loadtables='xc_novels')
-
-
 
 engine = create_engine('your_sqlalchemy_conn')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-XCNovels = sqldb_lianzai.get_table('xc_novels') # the sqlalchemy orm class
+XCNovels = sqldb_lianzai.get_table('xc_novels')  # the sqlalchemy orm class
