a02171587b71f94fde3b62cc804582bd72d3475f
wanze <a358003542@gmail.com>
2020-08-23 16:48:04 +0800

    0.2.2

--- my_python_module/__init__.py
+++ my_python_module/__init__.py
@@ -3,6 +3,6 @@
 
 
 __softname__ = 'my_python_module'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 
--- my_python_module/basic/json.py
+++ my_python_module/basic/json.py
@@ -5,45 +5,60 @@
 import os
 import json
 
-"""
-treat json file as one dict value, and do some operation.
-"""
 
 def write_json(file, data):
+    """
+    write data to json file
+    """
     with open(file, 'w', encoding='utf8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
 
-def get_json_file(json_filename):
+def get_json_file(json_filename, default_data=None):
     """
-    :return:
+    try get json file or auto-create the json file with default_data
     """
+    default_data = default_data if default_data is not None else {}
+
     if not os.path.exists(json_filename):
-        data = {}
+        data = default_data
         write_json(json_filename, data)
 
     return json_filename
 
 
-def get_json_data(json_filename):
+def get_json_data(json_filename, default_data=None):
     """
     get json file data
-    :return:
     """
-    with open(get_json_file(json_filename), encoding='utf8') as f:
-        res = json.load(f)
-        return res
+    with open(get_json_file(json_filename, default_data=default_data),
+              encoding='utf8') as f:
+        data = json.load(f)
+        return data
 
 
-def get_json_value(json_filename, k):
-    res = get_json_data(json_filename)
-    return res.get(k)
-
-
-def set_json_value(json_filename, k, v):
+def get_json_value(json_filename, key, default_data=None):
     """
-    set json value on target file
+    get value by key in json file if your json file stored value as one dict.
     """
-    res = get_json_data(json_filename)
-    res[k] = v
-    write_json(get_json_file(json_filename), res)
+    data = get_json_data(json_filename, default_data=default_data)
+    if not isinstance(data, dict):
+        raise Exception(
+            "the target json file must stored whole data as one dict.")
+
+    return data.get(key)
+
+
+def set_json_value(json_filename, key, value, default_data=None):
+    """
+    set value by key and value in json file if your json file stored value
+    as one dict.
+    """
+    data = get_json_data(json_filename, default_data=default_data)
+
+    if not isinstance(data, dict):
+        raise Exception(
+            "the target json file must stored whole data as one dict.")
+
+    data[key] = value
+    write_json(get_json_file(json_filename), data)
