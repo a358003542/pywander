0981a466fbf471f8f52546c488358cedb7c6377a
wanze <a358003542@outlook.com>
2025-06-07 16:02:06 +0800

    update

--- pywander/image/convert_image.py
+++ pywander/image/convert_image.py
@@ -8,10 +8,10 @@ import shutil
 
 from PIL import Image
 
+from pywander.unique_key import random_md5
 from pywander.path import mkdirs
-from pywander.exceptions import PdftocairoProcessError, PillowProcessError, \
-    InkscapeProcessError
-from .utils import detect_output_file_exist, fix_filename_encoding_problem
+from pywander.exceptions import PdftocairoProcessError, PillowProcessError, InkscapeProcessError
+from .utils import detect_output_file_exist
 
 logger = logging.getLogger(__name__)
 
@@ -49,21 +49,26 @@ def convert_image_by_inkscape(input_img, outputdir, output_img_name, outputforma
     if input_img == output_img:
         raise FileExistsError
 
+    inkscape_command = 'inkscape'
     if shutil.which('inkscape'):
-        process_cmd = ['inkscape', f'--export-type={outputformat}', '-d', str(dpi), input_img]
-        logger.debug(f'start call cmd {process_cmd}')
-        subprocess.check_call(process_cmd)
-        # only retcode is zero
-        return output_img
+        pass
+    elif shutil.which('inkscape', path='C:\\Program Files\\Inkscape\\bin'):
+        inkscape_command = shutil.which('inkscape', path='C:\\Program Files\\Inkscape\\bin')
     else:
         raise InkscapeProcessError("inkscape command not found.")
 
+    process_cmd = [inkscape_command, f'--export-type={outputformat}', '-d', str(dpi), input_img]
+    logger.debug(f'start call cmd {process_cmd}')
+    subprocess.check_call(process_cmd)
+    return output_img
 
-def convert_image_by_pdftocairo(input_img, outputdir, output_img_name,
-                                outputname='', outputformat='png',
-                                overwrite=True,
-                                dpi=150, pdftocairo_fix_encoding='',
-                                transparent=False):
+
+def convert_image_by_pdftocairo(input_img, outputdir, output_img_name, outputname='', outputformat='png',
+                                overwrite=True, dpi=150, transparent=False):
+    """
+    pdftocairo对中文名文件支持不太好，先输出到临时文件，再更改名字
+
+    """
     output_img = detect_output_file_exist(outputdir, output_img_name, outputformat, overwrite)
     if not output_img:
         return None
@@ -79,38 +84,40 @@ def convert_image_by_pdftocairo(input_img, outputdir, output_img_name,
 
     out_flag = map_dict[outputformat]
 
-    try:
-        if outputformat in ['png', 'jpeg']:
-            # png jpeg without ext so use the output_img_name
-            process_cmd = ['pdftocairo', out_flag, '-singlefile', '-r', str(dpi),
-                           input_img, output_img_name]
+    temp_out_name = 'temp_' + random_md5(limit=10)
+    temp_out_filename = f'{temp_out_name}.{outputformat}'
 
-            if transparent and outputformat == 'png':
-                process_cmd.insert(2, '-transp')
+    if outputformat in ['png', 'jpeg']:
+        # png jpeg without ext so use the output_img_name
+        process_cmd = ['pdftocairo', out_flag, '-singlefile', '-r', str(dpi),
+                       input_img, temp_out_name]
 
-            logger.debug(f'start call cmd {process_cmd}')
-            subprocess.check_call(process_cmd)
+        if transparent and outputformat == 'png':
+            process_cmd.insert(2, '-transp')
 
-            fix_filename_encoding_problem(output_img_name,
-                                          outputformat,
-                                          overwrite=overwrite,
-                                          pdftocairo_fix_encoding=pdftocairo_fix_encoding)
-        else:
-            process_cmd = ['pdftocairo', out_flag, '-r', str(dpi), input_img,
-                           outputname]
-            if transparent and outputformat == 'tiff':
-                process_cmd.insert(2, '-transp')
+        logger.debug(f'start call cmd {process_cmd}')
+        subprocess.check_call(process_cmd)
 
-            logger.debug(f'start call cmd {process_cmd}')
-            subprocess.check_call(process_cmd)
-    finally:
-        os.chdir(cur_dir)
-        return output_img
+    else:
+        process_cmd = ['pdftocairo', out_flag, '-r', str(dpi), input_img,
+                       temp_out_filename]
+        if transparent and outputformat == 'tiff':
+            process_cmd.insert(2, '-transp')
+
+        logger.debug(f'start call cmd {process_cmd}')
+        subprocess.check_call(process_cmd)
+
+    if overwrite:
+        os.replace(temp_out_filename, output_img)
+    else:
+        os.rename(temp_out_filename, output_img)
+
+    os.chdir(cur_dir)
+    return output_img
 
 
 def convert_image(input_img, outputformat='png', dpi=150, outputdir='',
-                  outputname='', pdftocairo_fix_encoding='',
-                  overwrite=True, transparent=False):
+                  outputname='', overwrite=True, transparent=False):
     """
     - intput_img 输入图片
     - outputformat
@@ -153,7 +160,6 @@ def convert_image(input_img, outputformat='png', dpi=150, outputdir='',
         elif img_ext[1:] in ['pdf'] and outputformat in ['png', 'jpeg', 'ps', 'eps', 'svg']:
             return convert_image_by_pdftocairo(input_img, outputdir, output_img_name, outputname=outputname,
                                                outputformat=outputformat, dpi=dpi, overwrite=overwrite,
-                                               pdftocairo_fix_encoding=pdftocairo_fix_encoding,
                                                transparent=transparent)
     except PillowProcessError as e:
         logger.error(e)
